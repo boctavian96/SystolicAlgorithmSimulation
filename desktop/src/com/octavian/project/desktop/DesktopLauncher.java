62bd85b01388bf1b3f551ffcb392b2dfcf09fb87
boctavian96 <boctavian96@yahoo.com>
2019-01-05 13:16:24 +0200

    First commit

---
+++ desktop/src/com/octavian/project/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.octavian.project.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.octavian.project.Systolic;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Systolic(), config);
+	}
+}
